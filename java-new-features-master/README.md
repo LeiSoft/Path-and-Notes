bc6e5b06f24e3538df0ea566571d314d101a93d8
Quorra <923158428@qq.com>
2022-09-05 08:34:06 +0800

    :rocket:​java5-18特性分析

---
+++ java-new-features-master/README.md
@@ -0,0 +1,28 @@
+![图片展示](./resources/Java%E6%96%B0%E7%89%B9%E6%80%A7.png)
+
+# java-new-features
+Java新特性 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
+
+## 项目目标
+1. 列举Java5-17版本的关键新特性
+2. 对每个新特性进行全面的阐述与代码示例
+
+## 新特性目标(为什么需要不断升级)
+- 目标：速度更快，代码更少，更加安全
+- 原因：语言必须发展，否则它们就有变得无关紧要的风险。 -- Brian Goetz
+
+## 官网新特性
+- [OracleJDK](https://www.oracle.com/java/technologies/javase/jdk-relnotes-index.html)
+- [OpenJDK ](http://openjdk.java.net/projects/jdk/)
+
+# JCP/JEP/JSR
+- JCP: [Java Community Process](https://www.jcp.org/en/home/index)
+  * Java社区进程
+  * JCP成立于1998年, 由社会各界Java组成的社区, 规划和领导Java的发展 
+- JEP: [JDK Enhancement Proposals](http://openjdk.java.net/jeps/0) 
+  * jdk改进提案, 非正式的规范
+  * OpenJDK社区搜集增强jdk提案的过程
+- JSR: [Java Specification Requests](https://www.jcp.org/en/jsr/platform)
+  * java规范请求, 标准化技术规范的正式请求
+  * 由JCP成员向委员会提交的Java发展提案, 经过一系列流程后, 如果通过最终会体现在未来的Java中
+
---
+++ java-new-features-master/src/com/hepengju/java18/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * Java18的新特性
+ *
+ * <pre>
+ *      400: UTF-8 by Default                                 默认UTF8字符集
+ *      408: Simple Web Server                                简单Web服务器
+ *      413: Code Snippets in Java API Documentation          简化文档代码片段
+ *      416: Reimplement Core Reflection with Method Handles  重新实现反射核心机制（Method Handles）
+ *      417: Vector API (Third Incubator)                     矢量API（三次孵化中）
+ *      418: Internet-Address Resolution SPI                  互联网地址解析SPI
+ *      419: Foreign Function & Memory API (Second Incubator) 外部函数和内存 API（第二孵化器）
+ *      420: Pattern Matching for switch (Second Preview)     switch 模式匹配（二次预览）
+ *      421: Deprecate Finalization for Removal               弃用Finalization
+ * </pre>
+ *
+ * @see <a href="http://openjdk.java.net/projects/jdk/18/">OpenJDK18</a>
+ * @see <a href="https://www.oracle.com/java/technologies/javase/18-relnote-issues.html">OracleJDK18</a>
+ *
+ * @see <a href="https://blog.csdn.net/qq_26420601/article/details/123728907">走进JDK18新特性</a>
+ *
+ * @author hepengju
+ */
+package com.hepengju.java18;
